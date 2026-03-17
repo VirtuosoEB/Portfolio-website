db86923719b20219dcc4169618d759b7e7b38896
Elijah Bienvenue <bienvenueelijah@gmail.com>
2026-03-16 21:51:27 -0400

    making things more professional

--- main.js
+++ main.js
@@ -11,3 +11,43 @@ const observer = new IntersectionObserver((entries) => {
 document.querySelectorAll(".fade-in").forEach(el => {
   observer.observe(el);
 });
+
+const sections = document.querySelectorAll("section");
+const backgrounds = document.querySelectorAll("#bgContainer img");
+
+const observer2 = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+
+    if (entry.isIntersecting) {
+
+      const index = [...sections].indexOf(entry.target);
+
+      backgrounds.forEach(bg => bg.classList.remove("active"));
+      backgrounds[index].classList.add("active");
+
+    }
+
+  });
+}, {
+  rootMargin: "-30% 0px -30% 0px",
+  threshold: 0
+});
+
+sections.forEach(section => observer2.observe(section));
+
+const speedbtn = document.getElementById("speedBtn");
+const skillContainer = document.getElementById("skillContainer");
+
+const caseStudies = document.querySelectorAll(".caseStudy");
+caseStudies.forEach(el => {
+    el.querySelector("button").addEventListener("click", () =>{
+        el.style.display = "none";
+    })
+});
+
+const projects = document.querySelectorAll(".project");
+projects.forEach((el, index) => {
+    el.querySelector(".viewCaseStudy").addEventListener("click", () =>{
+        document.querySelectorAll(".caseStudy")[index].style.display = "block"
+    })
+});
